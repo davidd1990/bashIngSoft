0b7df35965e515d73aa65c7c934557133b5246c2
JUan DAvid <jibaneze@unbosque.edu.co>
2019-03-27 12:55:11 -0500

    actualizacion bash panel colores

--- panelcontrol.sh
+++ panelcontrol.sh
@@ -1,18 +1,68 @@
+bold=`echo -en "\e[1m"`
+underline=`echo -en "\e[4m"`
+dim=`echo -en "\e[2m"`
+strickthrough=`echo -en "\e[9m"`
+blink=`echo -en "\e[5m"`
+reverse=`echo -en "\e[7m"`
+hidden=`echo -en "\e[8m"`
+normal=`echo -en "\e[0m"`
+black=`echo -en "\e[30m"`
+red=`echo -en "\e[31m"`
+green=`echo -en "\e[32m"`
+orange=`echo -en "\e[33m"`
+blue=`echo -en "\e[34m"`
+purple=`echo -en "\e[35m"`
+aqua=`echo -en "\e[36m"`
+gray=`echo -en "\e[37m"`
+darkgray=`echo -en "\e[90m"`
+lightred=`echo -en "\e[91m"`
+lightgreen=`echo -en "\e[92m"`
+lightyellow=`echo -en "\e[93m"`
+lightblue=`echo -en "\e[94m"`
+lightpurple=`echo -en "\e[95m"`
+lightaqua=`echo -en "\e[96m"`
+white=`echo -en "\e[97m"`
+default=`echo -en "\e[39m"`
+BLACK=`echo -en "\e[40m"`
+RED=`echo -en "\e[41m"`
+GREEN=`echo -en "\e[42m"`
+ORANGE=`echo -en "\e[43m"`
+BLUE=`echo -en "\e[44m"`
+PURPLE=`echo -en "\e[45m"`
+AQUA=`echo -en "\e[46m"`
+GRAY=`echo -en "\e[47m"`
+DARKGRAY=`echo -en "\e[100m"`
+LIGHTRED=`echo -en "\e[101m"`
+LIGHTGREEN=`echo -en "\e[102m"`
+LIGHTYELLOW=`echo -en "\e[103m"`
+LIGHTBLUE=`echo -en "\e[104m"`
+LIGHTPURPLE=`echo -en "\e[105m"`
+LIGHTAQUA=`echo -en "\e[106m"`
+WHITE=`echo -en "\e[107m"`
+DEFAULT=`echo -en "\e[49m"`;
+title="Escoja una opcion"
+fin="Seleccione una opcion [1 - 6]"
+columns=$(tput cols)
 clear
 while :
 do
-echo " Escoja una opcion "
-echo "1. Ver ultimos comandos del usuario"
+	echo "${DEFAULT}${red}"
+	printf "%*s\n" $(((${#title}+$columns)/2)) "$title"
+echo "${lightyellow}1. Ver ultimos comandos del usuario"
 echo "2. Gestion de procesos"
 echo "3. Gestion de backups"
 echo "4. Consultar numero de archivos y peso"
 echo "5. Gestionar Links Duros"
 echo "6. Salir"
-echo -n "Seleccione una opcion [1 - 6]"
+echo "${DEFAULT}${green}"
+printf "%*s\n" $(((${#fin}+$columns)/2)) "$fin"
 read opcion
 case $opcion in
-1) echo "los ultimos comandos escritos son:";
-	cat $(pwd)/.bash_history;;
+1) echo "${green}los ultimos comandos escritos son:";
+	echo "${BLACK}";
+	i=1
+	while read linea; do i=$((i++))  echo -e "$i) $linea\n" ; done < $HOME/.bash_history;;
+
 2) bash gestionprocesos.sh;;
 3) bash gestionbackup.sh;;
 4) echo "Digite la carpeta que desea verificar";read v ;bash listarfile.sh $v ;;
